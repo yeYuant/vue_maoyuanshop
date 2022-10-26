3e4fb2782ad7d5b5e11b7fb1a0f5dad4086a2aaf
yeYuant <tixiu2253@163.com>
2022-10-26 16:59:45 +0800

    完成了项目的全部开发

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,8 @@ module.exports = defineConfig({
   transpileDependencies: true,
   // 关闭eslint 代码矫正提示
   lintOnSave: false,
-
+  // 去除Vue打包后js目录下生成的一些.map文件，用于加速生产环境构建
+  productionSourceMap: false,
   // webpack进行服务代理（解决跨域问题）
   devServer: {
     proxy: {
